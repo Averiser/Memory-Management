1d8f6c1b75775e2bf44073e8c1ffb01a2d6f7e1b
Averiser <averiser@gmail.com>
2022-01-18 12:23:39 +0300

    fourth commit - closure captures

---
+++ Pages/06-ClosureCapture.xcplaygroundpage/Contents.swift
@@ -0,0 +1,54 @@
+//: [Previous](@previous)
+
+//import Darwin
+
+var value = 0
+
+let show = {
+  print(value)
+}
+
+show()
+value = 10
+show()
+
+let show2 = { [value] in
+  print(value)
+}
+
+value = 11
+
+show2()
+value = 20
+
+show2()
+value = 30
+
+let show3 = { [v = value, b = value + 1] in
+  print(v, b)
+}
+show3()
+value = 40
+show3()
+
+
+class Cat {
+  func speak() {print("Meow")}
+}
+
+var play: () -> Void
+
+do {
+  let whiskers = Cat()
+
+  play = { [weak cat = whiskers] in
+    guard let cat = cat else {
+      print("no playing today")
+      return
+    }
+    cat.speak()
+  }
+  play()
+}
+
+play()
