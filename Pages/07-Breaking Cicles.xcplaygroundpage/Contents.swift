fd46654bf2769817e65fdc7ea4c29bb71a418127
Averiser <averiser@gmail.com>
2022-01-18 16:08:10 +0300

    fifth commit - breaking circles

---
+++ Pages/07-Breaking Cicles.xcplaygroundpage/Contents.swift
@@ -0,0 +1,48 @@
+//: [Previous](@previous)
+
+import Foundation
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+class Supernova: CelestialBody {
+  lazy var explode: () -> Void = { [unowned self] in
+    self.name = "BOOMED \(self.name)"
+  }
+}
+
+do {
+  let tycho = Supernova("tycho")
+  tycho.explode()
+  print(tycho.name)
+}
+
+do {
+  let kepler = Supernova("Kepler")
+  DispatchQueue.global().asyncAfter(deadline: .now() + 0.1) {
+    kepler.explode()
+    print(kepler.name)
+  }
+}
+
+//do {
+//  let galileo = Supernova("Galileo")
+//  DispatchQueue.global().asyncAfter(deadline: .now() + 0.1) {
+//    [unowned galileo] in
+//    galileo.explode()
+//    print(galileo.name)
+//  }
+//}
+
+do {
+  let galileo = Supernova("Galileo")
+  DispatchQueue.global().asyncAfter(deadline: .now() + 0.1) {
+    [weak galileo] in
+    guard let galileo = galileo else {
+      print("Nada")
+      return
+    }
+    galileo.explode()
+    print(galileo.name)
+  }
+}
